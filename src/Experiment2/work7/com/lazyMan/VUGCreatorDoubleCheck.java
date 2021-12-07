ae13e6988d5415e0d7ff8658f64d8d1e1cc0fbe8
yhy <1002543438@qq.com>
2021-12-07 21:15:44 +0800

    实验12——桥接模式模式实现游戏场景 Tips:刘伟男神说可以用抽象工厂方法，主要是针对游戏场景。但是这个桥接方法是我想的太复杂，可以不违背开闭原则情况下新增装备，修改XML配置文件，然后新建一个场景就可以重新生成了（XML的读取有点繁琐）

--- src/Experiment2/work7/com/lazyMan/VUGCreatorDoubleCheck.java
+++ src/Experiment2/work7/com/lazyMan/VUGCreatorDoubleCheck.java
@@ -9,6 +9,7 @@ package Experiment2.work7.com.lazyMan;
 public class VUGCreatorDoubleCheck {
 
     private volatile static VUGCreatorDoubleCheck instance = null;
+    private static Long time1, time2;
 
     private VUGCreatorDoubleCheck(){
 
@@ -18,10 +19,10 @@ public class VUGCreatorDoubleCheck {
         // 加双重判断是为了谁更快进入抢占生成实例，提高速度？
         if(instance == null){
             synchronized (VUGCreatorDoubleCheck.class){
-                Long time1 = System.currentTimeMillis();
+                time1 = System.currentTimeMillis();
                 if(instance == null){
                     instance = new VUGCreatorDoubleCheck();
-                    Long time2 = System.currentTimeMillis();
+                    time2 = System.currentTimeMillis();
                     System.out.println("双判断总共花费时间：" + (time2 - time1));
                 }
             }
--- src/Experiment2/work7/com/lazyMan/VUGCreatorDoubleCheck1.java
+++ src/Experiment2/work7/com/lazyMan/VUGCreatorDoubleCheck1.java
@@ -1,5 +1,7 @@
 package Experiment2.work7.com.lazyMan;
 
+import javafx.beans.binding.LongExpression;
+
 /**
  * @ClassName： VUGCreatorDoubleCheck1
  * @Description：
@@ -8,6 +10,7 @@ package Experiment2.work7.com.lazyMan;
  **/
 public class VUGCreatorDoubleCheck1 {
     private volatile static VUGCreatorDoubleCheck1 instance = null;
+    private static Long time1, time2;
 
     private VUGCreatorDoubleCheck1(){
 
@@ -17,10 +20,10 @@ public class VUGCreatorDoubleCheck1 {
 
         // 加双重判断是为了谁更快进入抢占生成实例，提高速度？
         synchronized (VUGCreatorDoubleCheck1.class){
-            Long time1 = System.currentTimeMillis();
+            time1 = System.currentTimeMillis();
             if(instance == null){
                 instance = new VUGCreatorDoubleCheck1();
-                Long time2 = System.currentTimeMillis();
+                time2 = System.currentTimeMillis();
                 System.out.println("单判断总共花费时间：" + (time2 - time1));
             }
         }
